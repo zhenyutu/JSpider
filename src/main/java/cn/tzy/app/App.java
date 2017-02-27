9820dbe630d66bd8d681e7676bcba280ae8d4564
zhenyutu <1572629866@qq.com>
2017-02-27 10:13:22 +0800

    添加maven框架

---
+++ src/main/java/cn/tzy/app/App.java
@@ -0,0 +1,13 @@
+package cn.tzy.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
